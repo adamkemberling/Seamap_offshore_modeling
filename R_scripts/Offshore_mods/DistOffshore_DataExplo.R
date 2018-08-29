89857ba748441ffa6b6344b5d9e98016d9b9a047
Adam A. Kemberling <adamkemberling@gmail.com>
2018-08-29 15:48:44 -0500

    made changes to how cpue was calculated and what trawl data was used, subset to just 40ft trawls but might change it back because we're missing a lot of crabs.

--- R_scripts/Offshore_mods/DistOffshore_DataExplo.R
+++ R_scripts/Offshore_mods/DistOffshore_DataExplo.R
@@ -10,9 +10,7 @@ trawl.info <- read_csv('~/Documents/KrackN/Seamap_man/Historic_cleaned/seamap_tr
 seamap <- read_csv('~/Documents/KrackN/Seamap_man/Historic_cleaned/seamap_sapidus_consolidation_2018.csv')
 
 
-# test
-
-#Lets rename things now that we are going to be doing more work besides merging
+####################  rename things  ####################
 names(seamap)
 seamap <- seamap %>% rename(CTD_Depth = DEPTH_EMAX,
                   Temp_S = TEMPSURF,
@@ -35,85 +33,74 @@ seamap <- seamap %>% rename(CTD_Depth = DEPTH_EMAX,
                   Date = MO_DAY_YR)
 
 
+###################  Restrict to just 40ft trawls  #############
+seamap <- seamap %>% filter(GEAR_TYPE == "ST",
+                            MESH_SIZE == 1.63) #mesh size for 4oft trawl, the main one
+
 seamap$Start_Lat <- as.numeric(as.character(seamap$Start_Lat)); seamap$Start_Long <- as.numeric(as.character(seamap$Start_Long))
 seamap <- seamap[complete.cases(seamap$Start_Long),]
 seamap <- seamap[complete.cases(seamap$Start_Lat),]
-#coordinates(seamap) <- ~Start_Long+Start_Lat
-
-
-gulf <- readOGR("~/Dropbox (The Craboratory)/The Craboratory/Kemberling/Tagging_calculations/Nos80_gulfcrop.shp") #study area shapefile
-crs(gulf)
-crs(seamap) <- crs(gulf)
 
 #coordinate references
 degree.crs <- crs("+proj=longlat +ellps=WGS84 +datum=WGS84") #Degree coordinate reference syster WGS 1984
 meters.crs <- crs("+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=6371007 +b=6371007 +units=m +no_defs")
 
-#spatial distribution
-plot(gulf, main = 'C. Sapidus Catch Locations')
-points(seamap[which(seamap$Sapidus_Catch > 0),], col = 'blue')
-bubble(seamap['Sapidus_Catch'], main = 'C. Sapidus Catch Locations' )
+###################  ggplot map polygons  #############
+map.world_polygon <- map_data("world")
 
-#distributions of environmental variables
-seamap <- as.data.frame(seamap)
-hist(seamap$Sapidus_Catch)
-hist(log(seamap$Sapidus_Catch)) #use poisson distribution
+#positive catches in 40ft trawls
+seamap %>% mutate(Crab_Caught = ifelse(Sapidus_Catch > 0, T, F),
+                  Crab_Caught = factor(Crab_Caught, levels = c(T,F))) %>%
+  filter(Crab_Caught == T) %>% 
+  ggplot(aes(x = Start_Long, y = Start_Lat)) +
+  coord_quickmap() +  # Define aspect ratio of the map, so it doesn't get stretched when resizing
+  coord_cartesian(xlim = c(-98.08, -79.95),ylim = c(24.8, 31.2)) +
+  theme_classic() +  # Remove ugly grey background
+  theme(legend.position = "bottom") +  # Position the legend at the top of the plot
+  geom_polygon(data = map.world_polygon, aes(x = long, y = lat, group = group)) +
+  geom_point(aes(color = Crab_Caught), alpha = .35) +
+  xlab("Longitude") +
+  ylab("Latitude") 
 
 
-hist(seamap$DO)
-plot(seamap$DO,seamap$Sapidus_Catch)
-hist(seamap$Salinity)
-plot(seamap$Salinity,seamap$Sapidus_Catch)
-hist(seamap$Temp)
-plot(seamap$Temp,seamap$Sapidus_Catch)
-hist(seamap$Start_Depth)
-plot(seamap$Start_Depth,seamap$Sapidus_Catch)
 
-summary(seamap$GEAR_TYPE)
+#################  Get Catch per hectare, 1kt = 1.852km/h  ############
+seamap <- seamap %>%  filter(MIN_FISH < 60) %>%     #remove any super long trawls
+  mutate(Spd_kmh = VESSEL_SPD * 1.852,              #convert knots to km/h
+         Area_km = ((MIN_FISH/60) * Spd_kmh) * .012192, #12.192m = 40ft
+         Area_Hectares = Area_km * 100,             #1km sq = 100 hectares
+         CPUE_towspd = Sapidus_Catch/Area_Hectares) #Crabs/hectare based on two speed and minutes fished
 
-#restrict to standard trawl only and get cpue, ST is shrimp trawl maybe use all trawling
-#trawl codes
-trawl.codes <- c('ST','FT')
-#shrimp traw;, fish trawl
-
-seamap <- seamap[which(seamap$GEAR_TYPE %in% trawl.codes),]
-#standardize to 30min trawls
-
-seamap$CPUE <- seamap$Sapidus_Catch / (seamap$MIN_FISH/30)
 
 #cpue points per year
-plot(seamap$Year, seamap$CPUE)
-tapply(seamap$CPUE, seamap$Year, summary)
+plot(seamap$Survey_Year, seamap$CPUE_towspd)
 
-#subset so no missing NA's
-seamap <- seamap[which(!is.na(seamap$CPUE) == TRUE),]
+#subset so no NA's
+seamap <- seamap[which(!is.na(seamap$CPUE_towspd) == TRUE),]
 
-#cpue yearly average
+
+
+#CPUE_towspd yearly average
 library(data.table) # You'll also need to install this package
-DT = data.table(as.data.frame(seamap))# Convert data.frame to data.table
-catch_timeline <- DT[,mean(CPUE), by = Year]
-catch_sd <- DT[,sd(CPUE), by = Year]
+DT <- data.table(as.data.frame(seamap))# Convert data.frame to data.table
+catch_timeline <- DT[,mean(CPUE_towspd), by = Survey_Year]
+catch_sd <- DT[,sd(CPUE_towspd), by = Survey_Year]
 
 # sort by year
-catch_timeline <- catch_timeline[order(Year),] 
-catch_timeline <- merge(catch_timeline, catch_sd, by = 'Year')
+catch_timeline <- catch_timeline[order(Survey_Year),] 
+catch_timeline <- merge(catch_timeline, catch_sd, by = 'Survey_Year')
 
 colnames(catch_timeline)[2] <- "CPUE"
 colnames(catch_timeline)[3] <- "sd"
 
-plot(CPUE ~ Year, data = catch_timeline, type = 'l',ylim=range(c(0, catch_timeline$CPUE+catch_timeline$sd)),
-     ylab = 'Mean catch / 30min trawl', main = 'Seamap C. sapidus sampling')
+plot(CPUE ~ Survey_Year, data = catch_timeline, type = 'l',ylim=range(c(0, catch_timeline$CPUE+catch_timeline$sd)),
+     ylab = 'Mean catch / Hectare', main = 'Seamap C. sapidus sampling')
 
-points(catch_timeline$Year, catch_timeline$CPUE,ylim=range(c(0, catch_timeline$CPUE+catch_timeline$sd)),
+points(catch_timeline$Survey_Year, catch_timeline$CPUE,ylim=range(c(0, catch_timeline$CPUE+catch_timeline$sd)),
        pch=19, xlab="Year", ylab="Mean CPUE +/- SD",
        main="CPUE and its Overdispersion")
 # hack: we draw arrows but with very special "arrowheads"
-arrows(catch_timeline$Year, 0, catch_timeline$Year, catch_timeline$CPUE+catch_timeline$sd, length=0.05, angle=90, code=3)
-
-
-
-#create relief column
-seamap$relief <- abs(seamap$Start_Depth - seamap$End_Depth)
+arrows(catch_timeline$Survey_Year, 0, catch_timeline$Survey_Year, catch_timeline$CPUE+catch_timeline$sd, length=0.05, angle=90, code=3)
 
 
 #### Get Distance Offshore column ############
@@ -137,6 +124,9 @@ project.shapefile<-spTransform(project.shapefile, crs(meters.crs)) # Convert CRS
 seamap <- seamap[which(seamap$Start_Long > -97.63193),]
 seamap <- seamap[which(seamap$Start_Long < 0),] #remove coordinate error
 
+
+
+
 coordinates(seamap)<- ~ Start_Long + Start_Lat # converts the file to a spatialPoints object
 crs(seamap) <-crs(degree.crs)
 seamap <-spTransform(seamap, crs(meters.crs)) # transforms to meters
@@ -148,6 +138,7 @@ plot(project.shapefile);points(seamap, col = 'lightblue')
 
 
 #calculate distance from shore
+seamap$distance_offshore <- rep(NA, nrow(seamap))
 for(i in 1:nrow(seamap)){
   seamap$distance_offshore[i] <- gDistance(seamap[i,], project.shapefile)
 }
